4d2ac0f5617605c75ad28f047f6bce0e32dd1225
garu097 <garu097@gmail.com>
2022-10-18 18:44:54 +0700

    adjust file config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 import {Introduce} from "@/src/components/layout/Introduce";
 import {About} from "@/src/components/layout/About";
 import {Contact} from "@/src/components/layout/Contact";
